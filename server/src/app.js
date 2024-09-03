f6ea254467bdb88cfa312bf8076b150481f008c6
Khadim Hussain <khadimkalhoro1@gmail.com>
2024-09-03 08:57:03 +0500

    minor changes

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
@@ -15,6 +21,9 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 
 // routes declarations
+app.get("/", (req, res) => {
+  res.json("Hello World!");
+});
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 
